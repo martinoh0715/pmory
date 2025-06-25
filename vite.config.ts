831b16cb42b7119b4e39dc04802d19b46a744407
martinoh0715 <martinoh0715@gmail.com>
2025-06-25 16:00:43 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
